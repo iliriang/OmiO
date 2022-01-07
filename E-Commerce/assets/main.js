3a3b1ad498aa1001ecca9728cf8cc942e10bf0a8
iliriang <89780234+iliriang@users.noreply.github.com>
2022-01-07 18:30:50 +0100

    Add files via upload

---
+++ E-Commerce/assets/main.js
@@ -0,0 +1,62 @@
+/*Menu Show */
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+/*  Remove Menu */
+
+const navLink = document.querySelectorAll(".nav__link"),
+  navMenu = document.getElementById("nav-menu");
+
+function linkAction() {
+  navMenu.classList.remove("show");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/* Scroll Sections Active Link */
+
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", scrollActive);
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  });
+}
+
+window.onscroll = () => {
+  const nav = document.getElementById("header");
+  if (this.scrolly >= 200) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+};
+
+const sizes = document.querySelectorAll(".size__tallas");
+
+function changeSize() {
+  sizes.forEach((size) => size.classList.remove("active"));
+  this.classList.add("active");
+}
+
+sizes.forEach((size) => size.addEventListener("click", changeSize));
